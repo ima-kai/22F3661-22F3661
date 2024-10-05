1bff1e6bb03f439c550849403ece4758238526ee
Imama-Kainat <imamakainat9@gmail.com>
2024-10-05 16:27:20 +0500

    Added Main class to test Library functions

---
+++ src/i22F366122F3661k/MAIN.java
@@ -0,0 +1,11 @@
+package i22F366122F3661k;
+
+public class MAIN {
+    public static void main(String[] args) {
+        libraryclass myLibrary = new libraryclass ();
+        myLibrary.addBook("Harry Potter");
+        myLibrary.addBook("Lord of the Rings");
+        myLibrary.listBooks();
+    }
+}
+
